425a1ec715de6913788711d3afb391ab69a865f6
rafaelcoias <rafaelmsppcoias@gmail.com>
2023-09-01 19:45:35 +0100

    update

--- pages/newsletter/script.js
+++ pages/newsletter/script.js
@@ -11,7 +11,24 @@ function loadDataPage(language) {
 }
 
 function submit() {
+  let email = document.getElementById('newsletter-form-input');
+  let msg = document.getElementById('newsletter-form-msg-text');
+  let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
+  if (email.value === "" || !email.value.match(emailPattern)) {
+    // Mensagem de erro
+    msg.innerHTML = 'Por favor escreva um email válido';
+    msg.style.color = 'red';
+    return ;
+  }
+  
+  // Guardar email
+  
+  // Mensagem de sucesso
+  msg.style.color = 'black';
+  msg.innerHTML = 'Subscrição registada. Verifique o seu email para confirmar.';
+  setTimeout(() => msg.innerHTML = '', 6000);
+  email.value = '';
 }
 
 loadDataPage(getSelectedLanguage());
