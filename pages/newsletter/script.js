531fdaaceffb7e265832b855503769bcd860812d
rafaelcoias <rafaelmsppcoias@gmail.com>
2023-09-01 19:11:35 +0100

    update

--- pages/newsletter/script.js
+++ pages/newsletter/script.js
@@ -1,21 +1,17 @@
-function updateContentPage(language, data) {
-    const translations = data[language];
-    
-    let textAll = document.querySelectorAll('.newsletter-title');
-    textAll.forEach((text, index) => {
-      text.innerHTML = translations.titles[index];
-    });
+function loadDataPage(language) {
+  if (language === "pt") {
+    document.getElementById('newsletter-title').innerHTML = "Subscreve a nossa newsletter";
+    document.getElementById('newsletter-text').innerHTML = "Publicamos notícias e novidades sobre a APCH";
+    document.getElementById('newsletter-form-btn').innerHTML = "Enviar";
+  } else {
+    document.getElementById('newsletter-title').innerHTML = "Subscribe to our newsletter";
+    document.getElementById('newsletter-text').innerHTML = "We publish news and PAHH updates";
+    document.getElementById('newsletter-form-btn').innerHTML = "Send";
+  }
 }
 
-function loadDataPage(language) {
-    fetch('./text.json')
-      .then(response => response.json())
-      .then(data => {
-        updateContentPage(language, data);
-    })
-    .catch(error => {
-        console.error('Error fetching JSON file:', error);
-    });
+function submit() {
+
 }
 
 loadDataPage(getSelectedLanguage());
